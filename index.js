56a941d8c099704e84109ab40f48d9e369f04f2a
Ryan Yang <Ryanyang99@hotmail.com>
2018-03-22 14:47:24 -0700

    Upate

--- index.js
+++ index.js
@@ -3,4 +3,17 @@ const Nightmare = require('nightmare');
 const { readFileSync, writeFileSync } = require('fs');
 const numbers = readFileSync('./tesco-title-numbers.csv', 
   {encoding: 'utf8'}).trim().split('\n');
-console.dir(numbers);
+const START = 'https://eservices.landregistry.gov.uk/wps/portal/Property_Search';
+const getAddress = async id => {
+  console.log(`Now checking ${id}`);
+  const nightmare = new Nightmare({ show: true });
+// Go to initial start page, navigate to Detail search
+  try {
+    await nightmare
+      .goto(START)
+      .wait('.bodylinkcopy:first-child')
+      .click('.bodylinkcopy:first-child');
+  } catch(e) {
+    console.error(e);
+  }
+}
